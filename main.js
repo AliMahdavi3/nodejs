9b6bad932ecb906c762cf46a81a60478c04a97f8
AliMahdavi3 <alimahdavi30000@gmail.com>
2023-05-05 00:31:13 +0430

    commit-7

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-const os = require("os");
-const totalMemory = os.totalmem();
-const freeMemory = os.freemem();
-const hostName = os.hostname();
+const EventEmitter = require("events");
+const emitter = new EventEmitter;
 
-console.log(totalMemory);
-console.log(freeMemory);
-console.log(hostName);
+emitter.on("Phone Called", function(value){
+    console.log("Answer Phone Call From " + value.name);
+})
 
-// ================================================================\\
+process.on("exit", code => {
+    console.log("Phone Call From ", code);
+})
 
-const { copyFile } = require("fs");
-
-copyFile("sorece.txt", "destination.txt", function(err){
-    console.log(err);
+emitter.emit("Phone Called", {
+    id : 1, 
+    name : "Ali",
+    number : 09012559469,
 });
-// transform text from sorece.txt to desstination.txt
-// ================================================================\\
 
-const path = require("path");
-const file = path.parse(__filename);
-
-console.log(file);
-// showing values in file with parse method
-// root, dir, base, ext, name,
