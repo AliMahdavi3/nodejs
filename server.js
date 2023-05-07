929409ad30156d1196b5b9fb9e3faf6acf30a6da
AliMahdavi3 <alimahdavi30000@gmail.com>
2023-05-07 22:27:03 +0430

    commit-25

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const messageRouter = require("./routes/messageRouter");
 const friendsRouter = require("./routes/friendsRouter");
+const path = require("path");
 const PORT = 3001;
 
 app.use((req, res, next) => {
@@ -11,6 +12,17 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 app.use(express.json());
+
+app.set("view engine", "hbs");
+app.set("views", "views");
+app.get("/", (req, res) => {
+    res.render("index", {
+        title : "HBS Website!",
+        caption : "This is my site!"
+    });
+});
+
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use("/message", messageRouter);
 app.use("/friends", friendsRouter);
 
